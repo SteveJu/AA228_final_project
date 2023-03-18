cfb63ecac8b3894a6eae1f503b1c6f8eeb201f6e
mjiang9 <mayjiang@princeton.edu>
2023-03-18 12:09:03 -0400

    fixes for critic to run

--- CodeRL_copy/scripts/train_critic.sh
+++ CodeRL_copy/scripts/train_critic.sh
@@ -8,7 +8,7 @@
 # Run code in debugging mode (without deepspeed) 
 python \
     train.py \
-    --batch-size-per-replica 8 --grad-acc-steps 1 \
+    --batch-size-per-replica 4 --grad-acc-steps 1 \
     --epochs 10 --lr 2e-5 \
     --save-freq 1000 --log-freq 10 --save_total_limit 5 \
     --tuning_mode critic --model codet5-base \
